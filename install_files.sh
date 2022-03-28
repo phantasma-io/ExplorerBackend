0125ebe9543f65e02d5ae38bfbefd28bfa484613
mlthelama <lackner.martin79@gmail.com>
2022-03-28 18:34:16 +0200

    [FEATURE]
    * added systemd service files and install for it

--- install_files.sh
+++ install_files.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo systemctl stop data-fetcher.service
+sudo systemctl stop api-service.service
+
 sudo mkdir -p /opt/ExplorerBackend/
 sudo rm -r /opt/ExplorerBackend/*
 
@@ -8,3 +11,14 @@ sudo cp -r * /opt/ExplorerBackend/
 
 cd ../overwrite
 sudo cp * /opt/ExplorerBackend/
+
+cd ..
+sudo cp data-fetcher.service /usr/lib/systemd/system/
+sudo cp api-service.service /usr/lib/systemd/system/
+sudo systemctl daemon-reload
+
+sudo systemctl start data-fetcher.service
+sudo systemctl start api-service.service
+
+sudo systemctl enable data-fetcher.service
+sudo systemctl enable api-service.service
