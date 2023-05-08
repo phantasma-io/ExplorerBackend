bd165263f55b22a24252df0d4d261ebc1ae81a3a
João Novo <34069419+TeknoPT@users.noreply.github.com>
2023-05-08 14:07:29 +0100

    Update EP.Transaction.cs

--- Backend.Service.Api/EP.Transaction.cs
+++ Backend.Service.Api/EP.Transaction.cs
@@ -761,11 +761,11 @@ public partial class Endpoints
                 : null
         };
 
-        var events = with_events == 1
+        /*var events = with_events == 1
             ? await CreateEventsForTransaction(databaseContext, tx, with_nft, with_event_data, with_fiat, fiatCurrency, fiatPricesInUsd)
             : null;
 
-        transaction.events = events;
+        transaction.events = events;*/
 
         return transaction;
     }
