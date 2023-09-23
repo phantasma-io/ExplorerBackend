91457d48e2d0439c740ebfb8b20640b4694517f6
alexmpa <alexmpa@yandex.ru>
2023-09-22 22:33:12 -0300

    Fix crash when processing big decimals

--- Backend.Commons/Utils.cs
+++ Backend.Commons/Utils.cs
@@ -1,12 +1,17 @@
-using System.Globalization;
-using Phantasma.Core.Numerics;
+using System.Numerics;
 
 namespace Backend.Commons;
 
 public static class Utils
 {
+    // TDOD fix implementation of UnitConversion.ToDecimal() in Phantasma's code
     public static string ToDecimal(string amount, int tokenDecimals)
     {
-        return UnitConversion.ToDecimal(amount, tokenDecimals).ToString(CultureInfo.InvariantCulture);
+        if ( amount == "0" || tokenDecimals == 0 )
+            return amount;
+
+        var quotient = BigInteger.DivRem(BigInteger.Parse(amount), BigInteger.Pow(10, tokenDecimals), out var remainder);
+        
+        return quotient + "." + remainder;
     }
 }
