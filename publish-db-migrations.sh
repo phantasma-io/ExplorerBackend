9a735fcf80e0cc40e2e4100ddb48a3887ad3f164
alexmpa <alexmpa@yandex.ru>
2023-10-04 22:04:39 -0300

    Add migrations publish script

---
+++ publish-db-migrations.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+OUT_FOLDER=./publish
+OUT_BIN_FOLDER=$OUT_FOLDER/bin
+NET_SUBFOLDER=net7.0
+
+rm -r --force $OUT_FOLDER
+mkdir --parents $OUT_BIN_FOLDER
+
+cp -a Backend.Commons $OUT_BIN_FOLDER/
+rm -r $OUT_BIN_FOLDER/Backend.Commons/bin
+rm -r $OUT_BIN_FOLDER/Backend.Commons/obj
+
+cp -a Database.ApiCache $OUT_BIN_FOLDER/
+rm -r $OUT_BIN_FOLDER/Database.ApiCache/bin
+rm -r $OUT_BIN_FOLDER/Database.ApiCache/obj
+
+cp -a Database.Main $OUT_BIN_FOLDER/
+rm -r $OUT_BIN_FOLDER/Database.Main/bin
+rm -r $OUT_BIN_FOLDER/Database.Main/obj
