8138d9b64268fa3c441bb0b2abe99c7212d7c845
Alex <alexmpa@yandex.ru>
2024-07-05 19:20:35 -0300

    Fix publishing

--- publish-api.sh
+++ publish-api.sh
@@ -3,20 +3,19 @@
 OUT_FOLDER=./publish
 OUT_BIN_FOLDER=$OUT_FOLDER/bin
 NET_SUBFOLDER=net8.0
-CONFIGURATION=Release
 
 rm -r --force $OUT_FOLDER
 mkdir --parents $OUT_BIN_FOLDER
 mkdir --parents $OUT_BIN_FOLDER/Plugins # Not used by API atm
 
-cp -a Backend.Commons/bin/$CONFIGURATION/$NET_SUBFOLDER/*.dll $OUT_BIN_FOLDER
-cp -a Backend.Commons/bin/$CONFIGURATION/$NET_SUBFOLDER/*.pdb $OUT_BIN_FOLDER
-cp -a Backend.Commons/bin/$CONFIGURATION/$NET_SUBFOLDER/*.json $OUT_BIN_FOLDER
+cp -a Backend.Commons/bin/$NET_SUBFOLDER/*.dll $OUT_BIN_FOLDER
+cp -a Backend.Commons/bin/$NET_SUBFOLDER/*.pdb $OUT_BIN_FOLDER
+cp -a Backend.Commons/bin/$NET_SUBFOLDER/*.json $OUT_BIN_FOLDER
 
-cp -a Backend.Service.Api/bin/$CONFIGURATION/$NET_SUBFOLDER/*.dll $OUT_BIN_FOLDER
-cp -a Backend.Service.Api/bin/$CONFIGURATION/$NET_SUBFOLDER/*.pdb $OUT_BIN_FOLDER
-cp -a Backend.Service.Api/bin/$CONFIGURATION/$NET_SUBFOLDER/*.json $OUT_BIN_FOLDER
-cp -a Backend.Service.Api/bin/$CONFIGURATION/$NET_SUBFOLDER/*.xml $OUT_BIN_FOLDER
+cp -a Backend.Service.Api/bin/$NET_SUBFOLDER/*.dll $OUT_BIN_FOLDER
+cp -a Backend.Service.Api/bin/$NET_SUBFOLDER/*.pdb $OUT_BIN_FOLDER
+cp -a Backend.Service.Api/bin/$NET_SUBFOLDER/*.json $OUT_BIN_FOLDER
+cp -a Backend.Service.Api/bin/$NET_SUBFOLDER/*.xml $OUT_BIN_FOLDER
 
 cp -a explorer-backend-config.json $OUT_FOLDER
 cp -a start-api-service.sh $OUT_FOLDER
--- publish-worker.sh
+++ publish-worker.sh
@@ -3,37 +3,36 @@
 OUT_FOLDER=./publish
 OUT_BIN_FOLDER=$OUT_FOLDER/bin
 NET_SUBFOLDER=net8.0
-CONFIGURATION=Release
 
 rm -r --force $OUT_FOLDER
 mkdir --parents $OUT_BIN_FOLDER
 
-cp -a Backend.Commons/bin/$CONFIGURATION/$NET_SUBFOLDER/*.dll $OUT_BIN_FOLDER
-cp -a Backend.Commons/bin/$CONFIGURATION/$NET_SUBFOLDER/*.pdb $OUT_BIN_FOLDER
-cp -a Backend.Commons/bin/$CONFIGURATION/$NET_SUBFOLDER/*.json $OUT_BIN_FOLDER
+cp -a Backend.Commons/bin/$NET_SUBFOLDER/*.dll $OUT_BIN_FOLDER
+cp -a Backend.Commons/bin/$NET_SUBFOLDER/*.pdb $OUT_BIN_FOLDER
+cp -a Backend.Commons/bin/$NET_SUBFOLDER/*.json $OUT_BIN_FOLDER
 
-cp -a Backend.Service.Worker/bin/$CONFIGURATION/$NET_SUBFOLDER/*.dll $OUT_BIN_FOLDER
-cp -a Backend.Service.Worker/bin/$CONFIGURATION/$NET_SUBFOLDER/*.pdb $OUT_BIN_FOLDER
-cp -a Backend.Service.Worker/bin/$CONFIGURATION/$NET_SUBFOLDER/*.json $OUT_BIN_FOLDER
+cp -a Backend.Service.Worker/bin/$NET_SUBFOLDER/*.dll $OUT_BIN_FOLDER
+cp -a Backend.Service.Worker/bin/$NET_SUBFOLDER/*.pdb $OUT_BIN_FOLDER
+cp -a Backend.Service.Worker/bin/$NET_SUBFOLDER/*.json $OUT_BIN_FOLDER
 
 mkdir --parents $OUT_BIN_FOLDER/Plugins
-cp -a Backend.Api.Client/bin/$CONFIGURATION/$NET_SUBFOLDER/Backend.Api.Client.* $OUT_BIN_FOLDER/Plugins
-cp -a Backend.Plugins/Blockchain.Common/bin/$CONFIGURATION/$NET_SUBFOLDER/Blockchain.Common.* $OUT_BIN_FOLDER/Plugins
+cp -a Backend.Api.Client/bin/$NET_SUBFOLDER/Backend.Api.Client.* $OUT_BIN_FOLDER/Plugins
+cp -a Backend.Plugins/Blockchain.Common/bin/$NET_SUBFOLDER/Blockchain.Common.* $OUT_BIN_FOLDER/Plugins
 cp -a Backend.Plugins/Blockchain.Common/*.json $OUT_FOLDER
 cd Backend.Plugins/Blockchain.Phantasma
 dotnet publish
 cd -
-cp -a Backend.Plugins/Blockchain.Phantasma/bin/$CONFIGURATION/$NET_SUBFOLDER/Blockchain.Phantasma.* $OUT_BIN_FOLDER/Plugins
-cp -a Backend.Plugins/Blockchain.Phantasma/bin/$CONFIGURATION/$NET_SUBFOLDER/publish/Phantasma.* $OUT_BIN_FOLDER
+cp -a Backend.Plugins/Blockchain.Phantasma/bin/$NET_SUBFOLDER/Blockchain.Phantasma.* $OUT_BIN_FOLDER/Plugins
+cp -a Backend.Plugins/Blockchain.Phantasma/bin/$NET_SUBFOLDER/publish/Phantasma.* $OUT_BIN_FOLDER
 cp -a Backend.Plugins/Blockchain.Phantasma/*.json $OUT_FOLDER
-cp -a Backend.Plugins/Nft.TTRS/bin/$CONFIGURATION/$NET_SUBFOLDER/Nft.TTRS.* $OUT_BIN_FOLDER/Plugins
+cp -a Backend.Plugins/Nft.TTRS/bin/$NET_SUBFOLDER/Nft.TTRS.* $OUT_BIN_FOLDER/Plugins
 cp -a Backend.Plugins/Nft.TTRS/*.json $OUT_FOLDER
-cp -a Backend.Plugins/Price.CoinGecko/bin/$CONFIGURATION/$NET_SUBFOLDER/Price.CoinGecko.* $OUT_BIN_FOLDER/Plugins
+cp -a Backend.Plugins/Price.CoinGecko/bin/$NET_SUBFOLDER/Price.CoinGecko.* $OUT_BIN_FOLDER/Plugins
 cp -a Backend.Plugins/Price.CoinGecko/*.json $OUT_FOLDER
-cp -a Backend.Plugins/Price.ExchangeRatesApiIo/bin/$CONFIGURATION/$NET_SUBFOLDER/Price.ExchangeRatesApiIo.* $OUT_BIN_FOLDER/Plugins
+cp -a Backend.Plugins/Price.ExchangeRatesApiIo/bin/$NET_SUBFOLDER/Price.ExchangeRatesApiIo.* $OUT_BIN_FOLDER/Plugins
 cp -a Backend.Plugins/Price.ExchangeRatesApiIo/*.json $OUT_FOLDER
 
-cp -a Backend.Plugins/Blockchain.Img/bin/$CONFIGURATION/$NET_SUBFOLDER/Blockchain.Img.* $OUT_BIN_FOLDER/Plugins
+cp -a Backend.Plugins/Blockchain.Img/bin/$NET_SUBFOLDER/Blockchain.Img.* $OUT_BIN_FOLDER/Plugins
 cp -a Backend.Plugins/Blockchain.Img/*.json $OUT_FOLDER
 
 cp -a explorer-backend-config.json $OUT_FOLDER
